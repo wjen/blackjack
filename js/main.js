fb1d6d2d50057f5955e557e4241e821b0c9b4b51
wen jen <wjen85@gmail.com>
2016-05-19 10:23:57 -0700

    go over ace value and soft 17 redo

--- js/main.js
+++ js/main.js
@@ -4,11 +4,23 @@ var deck = [
   's02', 's03', 's04', 's05', 's06', 's07', 's08', 's09', 's10', 'sJ', 'sQ', 'sK', 'sA',
   'h02', 'h03', 'h04', 'h05', 'h06', 'h07', 'h08', 'h09', 'h10', 'hJ', 'hQ', 'hK', 'hA'
 ];
+var deckValues = {};
+
+deck.forEach(function(card) {
+  var faceValue  = card.substring(1);
+  var pointValue = parseInt(faceValue);
+  if (isNaN(pointValue)) { // J, Q, K, A
+    pointValue = (faceValue === 'A') ? 1 : 10;
+  }
+
+  deckValues[card] = pointValue;
+});
 
 var player1 = {
-              hand: [],
-              chips: 100
-              };
+  hand: [],
+  chips: 0,
+  dblDown: false
+};
 
 var dealer = [];
 
@@ -16,48 +28,36 @@ var playingDeck = null;
 
 var wager;
 
-var deckValues = {};
-
 var gameOutcome;
 
 var turn;
 
 
-// value of cards
-
-// deck.forEach(function(card) {
-//   var val = card.substring(1);
-//   if (isNaN(parseInt(val))) {
-//     val = (val === 'A') ? 1 : 10;
-//   }
-//    else {
-//     val = parseInt(val);
-//   }
-//   deckValues[card] = val;
-
-// });
-
-
-//check for 21
-// if (val === "A" && sumUp(player1.hand) > 21) {
-//       val = 1;
-//     }
-//     if (val === "A" && sumUp(dealer) > 21) {
-//       val = 1;
-//     }
-
-
 // GAME START
 function start() {
-
+  wager = 0;
+  player1.chips = 500;
+  clearHands();
+  gameOutcome = "";
 };
 
 
-
+//BET FUNCTION
+function bet(amount) {
+  if (amount) {
+      if(player1.chips >= amount) {
+        wager = amount;
+      } else {
+        console.log("you don't enough have chips")
+      }
+  } else {
+    wager = 0;
+  }
+}
 
 //DEAL
 function deal() {
-  clearHand();
+  clearHands();
   turn = "P1";
   gameOutcome = "playing";
   playingDeck = _.shuffle(deck);
@@ -70,54 +70,51 @@ function deal() {
   };
 
   // AUTOMATIC WINS
-  if(
-    //PLAYER BLACKJACK
-    ((sumUp(player1.hand) === 21 && sumUp(dealer) === 21) && (dealer[0] !== "A")) ||
-    //DEALER BLACKJACK
-    ((sumUp(player1.hand) !== 21 && sumUp(dealer) === 21))
-    ) {
-      checkWin();
+  if (sumUp(player1.hand) === 21 && (sumUp(dealer) === 21 && dealer[0] !== "A")) {
+        gameOutcome = "BLACKJACK"
+        player1.chips += wager*1.5;
+  } else if (sumUp(player1.hand) === 21 && sumUp(dealer) !== 21) {
+        gameOutcome = "BLACKJACK"
+        player1.chips += wager*1.5;
+  } else if (sumUp(player1.hand) !== 21 && sumUp(dealer) === 21) {
+        checkWin();
   }
 
 };
 
 
 // ADD UP VALUES
-function sumUp(arr) {
+function sumUp(hand) {
   var total = 0;
 
-  deck.forEach(function(card) {
-  var val = card.substring(1);
-  if (isNaN(parseInt(val))) {
-    val = (val === 'A') ? 1 : 10;
-  }
-   else {
-    val = parseInt(val);
-  }
-  deckValues[card] = val;
-
+  // sums up your hand...
+  hand.forEach(function(card) {
+    total += deckValues[card];
   });
 
-  arr.forEach(function(singleCard) {
-    total += deckValues[singleCard];
-  });
-  for(var i = 0; i < arr.length; i++) {
-    if  ((arr[i].indexOf("A") > -1) && (total < 11)) {
+  // iterates over your hand, looking for Aces, and adding 10
+  // to the value if they exist...
+  for(var i = 0; i < hand.length; i++) {
+    if ((hand[i][1] === "A") && (total <= 11)) {
       total += 10;
     }
   }
+
   return total;
 }
 
+//DOUBLE DOWN
+function dblDown() {
+  if((player1.chips > (wager * 2)) && (turn = "P1")) {
+    player1.dblDown = true;
+    hit(player1.hand);
+    stay();
 
-
-//CHECK FOR BUST
-function checkBust(pd) {
-  return (sumUp(pd) > 21);
-  // if(sumUp(pd) > 21) {
-  // gameOutcome = "Bust";
-  // clearHand();
-  // }
+    // After doing the win logic (inside stay).
+    player1.dblDown = false;
+  } else {
+    console.log("You don't have enough chips");
+  }
 }
 
 // HIT
@@ -128,56 +125,64 @@ function hit(hands) {
   } else if (sumUp(player1.hand) === 21) {
     stay();
   }
-  // checkBust(hands);
 }
 
 // STAY
 function stay() {
   turn = "Comp";
   //DEALER HITS ON UNTIL 17 AND ON SOFT 17
-  while((sumUp(dealer) < 17) || (dealer.length === 2 && dealer.join().indexOf("A") != -1))
-    {
+  while(
+    (sumUp(dealer) < 17) ||((dealer.length === 2) && (dealer.join().indexOf(6) != -1) && (dealer.join().indexOf("A") != -1))
+  ) {
     hit(dealer);
   }
-  // checkBust(dealer);
-  // gameOutcome = "game over";
   checkWin();
 }
 
-// check for winner
+//CHECK FOR BUST
+function checkBust(hand) {
+  return (sumUp(hand) > 21);
+}
+
+// CHECK FOR WINNER
 function checkWin() {
+  if (player1.dblDown) {
+    wager = wager * 2;
+  }
   if (checkBust(player1.hand)) {
     gameOutcome = "Dealer Wins";
+    player1.chips -= wager;
   } else if (checkBust(dealer)) {
     gameOutcome = "Player Wins";
+    player1.chips += wager;
   } else if (sumUp(dealer) > sumUp(player1.hand)) {
     gameOutcome = "Dealer Wins";
+    player1.chips -= wager;
   } else if (sumUp(dealer) < sumUp(player1.hand)) {
     gameOutcome = "Player Wins";
+    player1.chips += wager;
   } else if (sumUp(dealer) === sumUp(player1.hand)) {
     gameOutcome = 'Push';
+  }
+  // After outcome and payout, return wager back to default original wager
+  if (player1.dblDown) {
+    wager = wager / 2;
   }
 }
 
 // CLEAR HANDS RESET
-function clearHand() {
+function clearHands() {
   player1.hand = [];
   dealer = [];
 }
 
-// HELPER....
+// HELPER
 function renderGame() {
-console.log("Player hand: " + player1.hand);
-console.log("Dealer hand: " + dealer);
-console.log("Turn: " + turn);
-console.log("Game Outcome: " + gameOutcome);
+  console.log("Player hand: " + player1.hand);
+  console.log("Dealer hand: " + dealer);
+  console.log("Turn: " + turn);
+  console.log("Game Outcome: " + gameOutcome);
+  console.log("Chips: " + player1.chips);
+  console.log("Wager: " + wager);
+  console.log("DoubleDown: " + player1.dblDown)
 }
-
-
-
-
-
-
-
-
-
